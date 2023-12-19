01e921b4bd823ff11b99b80f7202619c64e4f689
ofaaoficial <oscarfamado@gmail.com>
2023-12-18 22:22:05 -0500

    feat: integration with dependency injection from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { SesSnsEventController } from './infraestructure/api/handlers/sesSnsEventController';
-import { MapperUseCase } from './application/useCases/mapper.useCase';
-import { SesSnsEventFactory } from './application/factory/sesSnsEvent.factory';
+import { SesSnsEventController } from './infraestructure/api/handlers/ses-sns-event.controller';
+import { SesSnsEventFactoryImpl } from './application/factory/ses-sns-event-factory-impl';
+import { MapperUseCaseImpl } from './application/use-cases/mapper-use-case-impl';
+import { SesSnsEventFactory } from './domain/services/ses-sns-event-factory.interface';
+import { MapperUseCase } from './domain/use-cases/mapper-use-case.interface';
 
 @Module({
-  imports: [],
   controllers: [SesSnsEventController],
-  providers: [MapperUseCase, SesSnsEventFactory],
+  imports: [],
+  providers: [
+    {
+      provide: SesSnsEventFactory,
+      useClass: SesSnsEventFactoryImpl,
+    },
+    {
+      provide: MapperUseCase,
+      useClass: MapperUseCaseImpl,
+    },
+  ],
 })
 export class AppModule {}
